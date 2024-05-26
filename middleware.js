1861b5b10a4a6f9f8cb9aa391f66097c90299a08
Mario Imanuel <mariodaruranto68@gmail.com>
2024-05-26 21:00:40 +0800

    Adding start script

--- middleware.js
+++ middleware.js
@@ -1,6 +1,5 @@
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
-    req.session.returnTo = req.originalUrl;
     res.send("You need to log-in first");
   }
   next();
